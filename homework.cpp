8fe1f028d68e7ceddedb87313007ed48e02ed685
sofia709 <vazquezsofi0519@gmail.com>
2024-12-19 21:45:19 +0000

    ll

--- homework.cpp
+++ homework.cpp
@@ -3,12 +3,21 @@
 
 int main()
 {
-    // Complete the homework problems here
     
-    // Problem 1
+    // Problem 1: Prompt the user for a number from 1 to 100
+    int number;
+    std::cout << "Please enter a number from 1 to 100: ";
+    std::cin >> number;
     
+    while (number < 1 || number > 100)
+    {
+         std::cout << "Invalid number entered. Please enter a number from 1 to 100: ";
+        std::cin >> number;
+    }
+    std::cout << "Thank you for your input!" << std::endl;
     
     // Problem 2
+    
     
     
     // Problem 3
